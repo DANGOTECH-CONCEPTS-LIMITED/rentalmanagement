cc9a52b0416fbd9979c9dab812a00b432422aa8f
Natalie-Abbie <abionatline@gmail.com>
2025-03-15 17:48:50 +0300

    modification to the gh-pages

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/rentalmanagement/frontend/" : "/", // Adjusted base path
+  base: mode === "production" ? "/rentalmanagement/frontend/" : "/", 
   server: {
     host: "::",
     port: 8080,
