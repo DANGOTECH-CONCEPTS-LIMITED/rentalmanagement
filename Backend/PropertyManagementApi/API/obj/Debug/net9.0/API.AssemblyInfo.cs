a1c0d6f052c2cb4474f4c380094b82fc0dd5710e
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2025-04-10 23:42:57 +0300

    Update binaries and assembly version to new build
    Updated binary files (API.dll, API.exe, API.pdb, apphost.exe) indicating changes in compiled code and debugging information. Updated AssemblyInformationalVersionAttribute in API.AssemblyInfo.cs to 1.0.0+840d37babb67731fff5adf38c69e67c493c78797. Updated API.AssemblyInfoInputs.cache reflecting changes in input cache. Updated API.sourcelink.json to point to new commit 840d37babb67731fff5adf38c69e67c493c78797.

--- Backend/PropertyManagementApi/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Backend/PropertyManagementApi/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223e86eb950888077519963a7638eaf2803cd690")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+840d37babb67731fff5adf38c69e67c493c78797")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
