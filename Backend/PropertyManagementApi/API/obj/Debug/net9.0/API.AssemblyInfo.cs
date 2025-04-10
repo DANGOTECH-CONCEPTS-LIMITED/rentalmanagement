8c4a41deb10b9bcb6953f207bf8ec56070a75f64
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2025-04-11 01:49:26 +0300

    updated code

--- Backend/PropertyManagementApi/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ Backend/PropertyManagementApi/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00ed5f7b20db8941b1b771cecf70428d39e2553")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0656b0c479897d5379a1686fe564f2660032a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
