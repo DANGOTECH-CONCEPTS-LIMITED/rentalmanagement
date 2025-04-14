d36780bf7a9f061005842d29da202fb1f016c0d8
NGOBI DANIEL <ngobizadokchrist@gmail.com>
2025-04-14 11:14:40 +0300

    Update .gitignore rules and clean up project files
    Updated .gitignore to include new rules for ignoring various
    directories, file types, and configuration files. Removed
    specific .vs directory paths and added a rule to ignore all
    .vs folders recursively. Added rules to ignore build output
    directories, Visual Studio Code and JetBrains Rider
    configuration files, Node.js dependencies, testing output,
    miscellaneous files, OS-generated files, and environment-
    specific configuration files.
    
    Removed multiple binary files, configuration settings, and
    build-related files from the project. Updated
    AssemblyInformationalVersionAttribute values and sourcelink
    references in the Application, Domain, and Infrastructure
    projects. Recompiled assemblies and updated dependencies.

--- Backend/PropertyManagementApi/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Backend/PropertyManagementApi/Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e857ba9ab63837d7fed01591948e78823204ed63")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2009581838c397730636ddf917dda8fcfe1e26b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Backend/PropertyManagementApi/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Backend/PropertyManagementApi/Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+039a6676c7270e39fb59f2f7487b6f0497770355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2009581838c397730636ddf917dda8fcfe1e26b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
