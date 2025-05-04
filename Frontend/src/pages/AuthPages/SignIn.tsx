83b3313f3c3c1fa0f3486f3eb76ef4ff09e4b51b
bigmosi <kinyeramo@gmail.com>
2025-05-04 12:55:38 +0300

    fix the issues

--- Frontend/src/pages/AuthPages/SignIn.tsx
+++ Frontend/src/pages/AuthPages/SignIn.tsx
@@ -55,7 +55,7 @@ const SignIn = () => {
           <div className="p-8">
             <div className="mb-8 text-center">
               <h1 className="text-2xl font-bold text-gray-900 mb-2">
-                Rental Hub
+                NYUMBA YO
               </h1>
               <p className="text-gray-600">
                 Your one stop property management solution
