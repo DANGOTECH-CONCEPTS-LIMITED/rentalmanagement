5698b39d5d6646559264292e77261f1be003706e
zadokchrist <ngobizadokchrist@gmail.com>
2025-09-22 13:10:07 +0300

    Remove RawResponse from UtilityPayments table
    This commit removes the `RawResponse` property from the `UtilityPayments` table in the database schema. The change is implemented in the migration file `20250922100947_RemoveResponseField.cs`, which uses the `DropColumn` method. Additionally, the `AppDbContextModelSnapshot.cs` file has been updated to reflect this removal, and the corresponding `20250922100947_RemoveResponseField.Designer.cs` file was generated to ensure the database context is in sync with the current schema.

---
+++ Backend/PropertyManagementApi/Infrastructure/Migrations/20250922100947_RemoveResponseField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveResponseField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RawResponse",
+                table: "UtilityPayments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RawResponse",
+                table: "UtilityPayments",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
